b2afe132d48cb90e5603a83591abd5f04bbcf917
Arthur <arthurjose2090@gmail.com>
2025-12-09 03:19:42 -0300

    Aula 28 PRONTA

--- JavaScript Basico - Para Iniciantes/Aula 28/js/script.js
+++ JavaScript Basico - Para Iniciantes/Aula 28/js/script.js
@@ -1,5 +1,12 @@
-const numero = prompt("Digite um Numero");
+const numero = Number(prompt("Digite um Numero"));
 const numeroTitulo = document.getElementById("numero-titulo");
 const texto = document.getElementById("texto");
 
 numeroTitulo.innerHTML = numero;
+texto.innerHTML = `<p>Seu numero -2 e: ${numero - 2}.</p>`
+texto.innerHTML += `<p>A raiz quadrada: ${numero ** 1/2}.</p>`
+texto.innerHTML += `<p>${numero} E inteiro?: ${Number.isInteger(numero)}.</p>`
+texto.innerHTML += `<p>${numero} E NaN?: ${Number.isNaN(numero)}.</p>`
+texto.innerHTML += `<p>Arredondando para BAIXO: ${Math.floor(numero)}.</p>`
+texto.innerHTML += `<p>Arredondando para CIMA: ${Math.ceil(numero)}.</p>`
+texto.innerHTML += `<p>Duas casas DECIMAIS: ${numero.toFixed(2)}.</p>`    
