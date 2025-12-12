593c97df04a277e70af4a2f2386e02227f0ad60b
Arthur <arthurjose2090@gmail.com>
2025-12-12 08:40:34 -0300

    Aula 30 FINALIZADA

--- JavaScript Basico - Para Iniciantes/aula30/index.js
+++ JavaScript Basico - Para Iniciantes/aula30/index.js
@@ -6,15 +6,15 @@
 // console.log(soma(2, 2));
 
 
-const raiz = function (n) {
-    return n ** 0.5;
-};
+// const raiz = function (n) {
+//     return n ** 0.5;
+// };
 
-console.log(raiz(9));
-console.log(raiz(16));
-console.log(raiz(25));
-console.log(raiz(36));
-console.log(raiz(42));
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+// console.log(raiz(36));
+// console.log(raiz(42));
 
 //Arrow function = FUNCTION NORMAL MAIS SIMPLIFICADA CIMA E BAIXO SAO IGUAL
 
