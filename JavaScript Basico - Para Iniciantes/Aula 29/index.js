d93e59a4bd8bd98f977c3b2b3b0cac8d5259b9b1
Arthur <arthurjose2090@gmail.com>
2025-12-12 06:10:18 -0300

    Aula 29 - Arrays

---
+++ JavaScript Basico - Para Iniciantes/Aula 29/index.js
@@ -0,0 +1,37 @@
+/*
+
+// const nome = "Arthur Jose";
+// console.log(nome[0]);
+
+[] apos a const nome faz com que o numero do caractere que ta dentro dos colchetes vai para output
+
+no exemplo acima
+console diria "A"
+*/
+
+
+const alunos =  ["Luiz", "Pedro", "Maria"];
+
+console.log(alunos)
+// console.log(alunos[0]); 
+// // nesse caso como o valor de const alunos e um array (qualquer coisa dentro de []) 
+// no exemplo a cima 0 seria o primeiro valor do array "Luiz" sendo assim o console log vai apresentar Luiz
+
+
+// console.log(alunos);
+// (alunos[0]) isso faz com que o valor que esta na posicao dos colchetes se modifique
+
+// console.log(alunos.length) 
+// para saber o tamanho do array
+
+// alunos.push("Fabricio")
+// alunos.push("Cleopatra")
+// adiciona ao final
+
+// alunos[4] = "Mario" 
+//adiciona tambem so que em posicao exata (se nao houver valor em [3] ele vai pular e definir valor no 4) (a empty item)
+
+// alunos.unshift("Caroline")
+// adiciona ao comeco
+
+console.log(alunos)
