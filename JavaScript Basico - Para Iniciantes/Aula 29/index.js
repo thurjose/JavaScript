a1c6cac4318dedf138906e52184766e48cbf5e25
Arthur <arthurjose2090@gmail.com>
2025-12-12 06:41:08 -0300

    Alunos intanceof Array

--- JavaScript Basico - Para Iniciantes/Aula 29/index.js
+++ JavaScript Basico - Para Iniciantes/Aula 29/index.js
@@ -12,7 +12,6 @@ console diria "A"
 
 const alunos =  ["Luiz", "Pedro", "Maria"];
 
-console.log(alunos)
 // console.log(alunos[0]); 
 // // nesse caso como o valor de const alunos e um array (qualquer coisa dentro de []) 
 // no exemplo a cima 0 seria o primeiro valor do array "Luiz" sendo assim o console log vai apresentar Luiz
@@ -33,5 +32,32 @@ console.log(alunos)
 
 // alunos.unshift("Caroline")
 // adiciona ao comeco
+
+// alunos.pop();
+// remove o ultimo elemento do array
+
+// const removido = alunos.pop()
+// resultado disso e oque foi removido
+
+// console.log(removido)
+//exibe o nome de quem foi removido
+
+// alunos.shift();
+//o primeiro elemento e removido
+
+// delete alunos[1];
+// deleta o elemento do indicie deixando-o como a empty item
+
+// console.log(alunos[50]);
+//mostra undefined como um indicie que nao existe
+
+// alunos.slice(0, 3)
+//fatiar o array nesse caso de 0 a 2
+// alunos.slice(0, -1)
+// faz o mesmo so que -1 seria (alunos.length)-1
+
+
+// console.log(alunos instanceof Array)
+//se true e array       else nao e array
 
 console.log(alunos)
